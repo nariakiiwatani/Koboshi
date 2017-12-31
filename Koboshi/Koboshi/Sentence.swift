9b1db12ed72217bcac542c087e873b7ab8e2143e
nariakiiwatani <nariakiiwatani@annolab.com>
2017-12-31 23:42:00 +0900

    SentenceとOperationの関係を見直し

--- Koboshi/Koboshi/Sentence.swift
+++ Koboshi/Koboshi/Sentence.swift
@@ -19,6 +19,7 @@ enum Condition
 		case succceed
 		case failed
 	}
+	case always
 	case application(path:String, type:appType)
 	case shellfile(file:String, type:shellType)
 	case shellscript(script:String, type:shellType)
@@ -33,13 +34,15 @@ enum Condition
 			case .isRunning: return !apps.isEmpty && apps[0].isFinishedLaunching
 			case .isNotRunning: return apps.isEmpty
 			}
+		case .always:
+			return true
 		default:
 			return true
 		}
 	}
 }
 
-enum Sentence {
+enum Operation {
 	case launch(path:String)
 	case activate(path:String)
 	case quit(path:String)
@@ -47,6 +50,7 @@ enum Sentence {
 	case shellString(script:String)
 	case open(file:String, app:String)
 	case wait(seconds:Int)
+	case through
 	func execute() -> Bool {
 		switch self {
 		case .launch(let path):
@@ -69,41 +73,65 @@ enum Sentence {
 		case .wait(let seconds):
 			Thread.sleep(forTimeInterval: TimeInterval(seconds))
 			return true
+		case .through:
+			return true
 		default:
 			return true
 		}
 	}
 }
 
+class Sentence {
+	var operation : Operation!
+	init(_ op:Operation) {
+		operation = op
+	}
+	func proc() -> Bool {
+		return operation.execute()
+	}
+}
+
+class SentenceWithCondition : Sentence {
+	var condition = Condition.always
+	var sentences = [Sentence]()
+	init() {
+		super.init(Operation.through)
+	}
+	override init(_ op:Operation) {
+		super.init(op);
+		assert(false);
+	}
+	override func proc() -> Bool {
+		if(condition.check()) {
+			for s in sentences {
+				if(!s.proc()) {
+					return false
+				}
+			}
+		}
+		return true
+	}
+}
+
+
 class Statement
 {
 	var timer : Timer!
-	var condition : Condition
-	var sentences : [Sentence]
+	var sentence = SentenceWithCondition()
 	
-	init() {
-		condition = Condition.application(path: "/Applications/QuickTime Player.app", type: .isNotRunning)
-		sentences = Array<Sentence>()
-		sentences.append(Sentence.launch(path:"/Applications/QuickTime Player.app"))
-		sentences.append(Sentence.wait(seconds:5))
-		sentences.append(Sentence.quit(path:"/Applications/QuickTime Player.app"))
-		sentences.append(Sentence.wait(seconds:5))
-		timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Statement.execute), userInfo: nil, repeats: true)
+	convenience init() {
+		self.init(path:"/Applications/QuickTime Player.app")
 	}
 	init(path:String) {
-		condition = Condition.application(path: path, type: .isNotRunning)
-		sentences = Array<Sentence>()
-		sentences.append(Sentence.launch(path:path))
-		sentences.append(Sentence.wait(seconds:5))
-		sentences.append(Sentence.quit(path:path))
-		sentences.append(Sentence.wait(seconds:5))
+		sentence.condition = Condition.application(path: path, type: .isNotRunning)
+		sentence.sentences = Array<Sentence>()
+		sentence.sentences.append(Sentence(Operation.launch(path:path)))
+		sentence.sentences.append(Sentence(Operation.wait(seconds:5)))
+		sentence.sentences.append(Sentence(Operation.quit(path:path)))
+		sentence.sentences.append(Sentence(Operation.wait(seconds:5)))
 		timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Statement.execute), userInfo: nil, repeats: true)
 	}
 	@objc func execute() {
-		if condition.check() {
-			sentences.forEach({ (s:Sentence) in
-				guard s.execute() else {return}
-			})
-		}
+		sentence.proc()
 	}
 }
