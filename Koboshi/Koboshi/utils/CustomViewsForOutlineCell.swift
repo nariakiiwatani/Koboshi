43a45924a052495ffe455cbebdfa4e0a0794d606
nariakiiwatani <nariakiiwatani@annolab.com>
2018-02-21 11:55:36 +0900

    comboに初期値を入力

--- Koboshi/Koboshi/utils/CustomViewsForOutlineCell.swift
+++ Koboshi/Koboshi/utils/CustomViewsForOutlineCell.swift
@@ -8,25 +8,65 @@
 
 import Cocoa
 
-protocol HasMultipulControls
+enum CustomControlType
 {
-	func addControl(control: NSControl)
+	case combo([String], String)
+	case browseFile(String)
 }
 
 class CustomCell : NSView
 {
 	var action: Selector?
 	weak var target: AnyObject?
-}
+	var resultFuncs = [()->String]()
+	var result: String {
+		return resultFuncs.reduce("", { str, elem  in
+			str+elem()
+		})
+	}
+	
+	func addControl(_ controlType: CustomControlType) {
+		switch controlType {
+		case let .combo(list, label):
+			let combo = NSComboBox(string: label)
+			combo.addItems(withObjectValues: list)
+			addControl(combo)
+			resultFuncs.append({ () -> String in
+				return combo.objectValueOfSelectedItem as! String
+			})
+		case let .browseFile(str):
+			let text = NSTextField(string: str)
+			let button = NSButton(title: "Browse...", target: self, action: #selector(CustomCell.browseFile(sender:)))
+			button.setValue(text, forKey: "target")
+			addControl(text)
+			addControl(button)
+			resultFuncs.append({ () -> String in
+				return text.stringValue
+			})
+		}
+	}
 
-extension CustomCell : HasMultipulControls
-{
+	func browseFile(sender:NSButton) {
+		let dialog = NSOpenPanel()
+		dialog.canChooseDirectories=false
+		dialog.canChooseFiles = true
+		dialog.canCreateDirectories = false
+		dialog.allowsMultipleSelection = false
+		dialog.begin { (result) -> Void in
+			if result == NSFileHandlingPanelOKButton {
+				guard let url = dialog.url else { return }
+				guard let target = sender.value(forKey: "target") as? NSControl else { return }
+				target.stringValue = url.path
+			}
+		}
+	}
+
 	func childActionOccured(_ sender:Any?) {
 		if let target = target {
 			_ = target.perform(action, with: self)
 		}
 	}
-	func addControl(control: NSControl) {
+	func addControl(_ control: NSControl) {
 		control.target = self
 		control.action = #selector(CustomCell.childActionOccured(_:))
 		addSubview(control)
